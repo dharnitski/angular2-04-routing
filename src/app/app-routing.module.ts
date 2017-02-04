5d8d5e81e7a3df0aca5335d0698e9e916ab5bc00
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-03 21:33:57 -0500

    Imperative Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { UserComponent } from './user/user.component';
 import { HomeComponent } from './home-component.component';
 import { Routes, RouterModule } from '@angular/router';
-import { ModuleWithProviders } from '@angular/core';
 
 const APP_ROUTES: Routes = [
     { path: 'user', component: UserComponent },
