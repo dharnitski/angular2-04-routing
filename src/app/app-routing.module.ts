a81e344b05ccb53c84f15db5a2264ba55c5dd9f8
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-04 11:04:17 -0500

    route redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { HomeComponent } from './home-component.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const APP_ROUTES: Routes = [
+    { path: 'user', redirectTo: '/user/1', pathMatch: 'full' },
     { path: 'user/:id', component: UserComponent },
     { path: 'user/:id', component: UserComponent, children: USER_ROUTES },
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: '**', redirectTo: '/user/1'},
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
